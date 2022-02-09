1767bc4fe85b9818b14d8481e3122e7340e82d85
Pascal <pppascallim@gmail.com>
2022-02-09 23:19:21 +0100

    Npm dependencies : react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" style={{fontFamily: "Arial"}}>
-      <h1>Bonjour ! </h1>
+      <h1>Bonjour ! aled </h1>
     </div>
   );
 }

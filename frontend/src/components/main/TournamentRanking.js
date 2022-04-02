ced31235fe12dd62d5a502ce7d1feb2eb33d9470
Pascal <pppascallim@gmail.com>
2022-04-02 14:39:26 +0200

    Now functional + tooltip working

--- frontend/src/components/main/TournamentRanking.js
+++ frontend/src/components/main/TournamentRanking.js
@@ -1,5 +1,8 @@
 import  Axios  from 'axios';
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import _ from 'lodash'
 
 import trophee from "../assets/trophee.png";
@@ -7,51 +10,170 @@ import trophee from "../assets/trophee.png";
 
 function TournamentRanking () {
 
+    const { gender } = useParams();
+
     const [tournaments, setTournaments] = useState([])
     const [tournamentsInfo, setTournamentsInfo] = useState([])
     const [players , setPlayers] = useState([])
     const [playerStats, setPlayerStats] = useState([])
-    const [activeCategory, setActiveCategory] = useState('')
+    const [activeCategory, setActiveCategory] = useState(0);
 
-    const [filteredPlayerStats, setFilteredPlayerStats] = useState([]) 
+
+    const [tournamentPlayersStatsSum , setTournamentPlayersStatsSum ] = useState([]);
+
+    // UseState pour fonction useCallback
+    const [ playerStatsByIdPlayer, setPlayerStatsByIdPlayer ] = useState([]);
+    const [ tournamentScoresSum, setTournamentScoresSum] = useState([]);
 
     useEffect(async () => {
         document.title = "InfoGolf - Classements par tournoi";
         
-        const result = await Axios.get('http://localhost:3001/tournamentRanking');
+        const result = await Axios.get(`http://localhost:3001/tournamentRanking/${gender}`);
 
         setTournaments(result.data.tournaments)
         setTournamentsInfo(result.data.tournamentsInfo)
         setPlayers(result.data.players)
         setPlayerStats(result.data.playersPoints)
-    }, [])
 
-    // Fonction qui trie par années les scores sur le tournoi en paramètre
-    function filterPlayerStatsByYear(idTournoi) {
-        const filteredPlayerStats = []
+    }, [setTournaments, setTournamentsInfo, setPlayers, setPlayerStats, gender, Axios])
+
+    const HtmlTooltip = styled(({ className, ...props }) => (
+        <Tooltip {...props} classes={{ popper: className }} />
+      ))(({ theme }) => ({
+        [`& .${tooltipClasses.tooltip}`]: {
+          backgroundColor: '#f5f5f9',
+          color: 'rgba(0, 0, 0, 0.87)',
+          maxWidth: 220,
+          fontSize: theme.typography.pxToRem(12),
+          border: '1px solid #dadde9',
+        },
+      }));
+      
+
+
+    const filteredPlayersScoreByYear = useMemo(() => {
+        const arrayStats = [];
 
         playerStats.map((array) => {
             array.map((player) => {
-                if(player.IdTournoi.includes(idTournoi)){
-                    filteredPlayerStats.push(player)
+                if(player.IdTournoi === (activeCategory)){
+                    arrayStats.push(player)
                 }
             })
         })
-
-        const sortedArray = _.groupBy(filteredPlayerStats, 'AnneeSaison');
-        const finalArray = _.values(sortedArray)
+        const sortedArray = _.groupBy(arrayStats, 'AnneeSaison');
+        const finalArray = _.values(sortedArray);
 
         return finalArray;
-    }
 
-    function handleRadio (tournament) {
-        setFilteredPlayerStats([]);
+      }, [playerStats, activeCategory],
+    )
 
-        const list = filterPlayerStatsByYear(tournament)
-        setFilteredPlayerStats(list);
+    // Scores triés par Id du joueur
+    const filteredScoresOfTournamentByIdPlayer = useMemo(() => {
+        const scoresGroupedByIdPlayer = []; 
 
-        setActiveCategory(tournament);
-    }
+        filteredPlayersScoreByYear.map((yearScores) => {
+            const groupedByIdPlayer = _.groupBy(yearScores, 'IdJoueur');
+            const flattenedArray = _.values(groupedByIdPlayer);
+            
+            scoresGroupedByIdPlayer.push(flattenedArray);
+        })
+
+        return scoresGroupedByIdPlayer;
+      },
+      [filteredPlayersScoreByYear]
+    )
+
+    // Somme des scores
+    const sumOfScores = useMemo(() => {
+        const sumScoresArray = [];
+
+        // Parcours des années
+        filteredScoresOfTournamentByIdPlayer.map((yearScores) => {
+
+            // Parcours des scores de l'année 
+            yearScores.map((playerScores) => {
+
+                var totalCoups = 0;
+                var totalPoints = 0;
+
+                // Parcours des scores pour faire la somme 
+                playerScores.map((score) => {
+                    totalCoups = totalCoups + score.nbCoups;
+                    totalPoints = totalPoints + score.nbPoints; 
+                }) 
+
+                sumScoresArray.push({
+                    IdJoueur: playerScores[0].IdJoueur,
+                    IdTournoi: playerScores[0].IdTournoi,
+                    AnneeSaison: playerScores[0].AnneeSaison,
+                    nbCoups: totalCoups,
+                    nbPoints: totalPoints,
+                })
+            })
+        })
+        const scoresGroupByYear = _.groupBy(sumScoresArray, 'AnneeSaison');
+        const finalScoresArray = _.values(scoresGroupByYear);
+
+        return finalScoresArray;
+    }, [filteredScoresOfTournamentByIdPlayer]
+    )
+
+    // Scores des jouers triés par jour (les scores du même jour ne sont pas encore additionnés)
+    const playerScoresSortedByDay = useMemo(() => {
+
+        const finalArray = []; 
+
+        filteredScoresOfTournamentByIdPlayer.map((yearScores) => {
+            yearScores.map((scores) => {
+                const scoresGroupedByDay = _.groupBy(scores, 'jour');
+                const dayScores = _.values(scoresGroupedByDay);
+                finalArray.push(dayScores);
+            })
+        })
+
+        return finalArray;
+    }, [filteredScoresOfTournamentByIdPlayer]
+    )
+
+    // console.log(playerScoresSortedByDay);
+
+    const playerScoresSortedByDayAdded = useMemo(() => {
+        const scoresArray = [];
+
+        playerScoresSortedByDay.map((playerScore) => {
+            playerScore.map((scoreOfDay) => {
+
+                var nbCoups = 0;
+                var nbPoints = 0;
+                var jour= 0;
+                var countScoresOfDay = 0;
+
+                scoreOfDay.map((scores) => {
+                    nbCoups = nbCoups + scores.nbCoups;
+                    nbPoints = nbPoints + scores.nbPoints;
+                    jour = scores.jour;
+                    countScoresOfDay = countScoresOfDay + 1 ;
+                })
+                scoresArray.push({
+                    jour: jour,
+                    nombreTrousJoues: countScoresOfDay,
+                    AnneeSaison: scoreOfDay[0].AnneeSaison,
+                    IdJoueur: scoreOfDay[0].IdJoueur,
+                    IdTournoi: scoreOfDay[0].IdTournoi,
+                    nbCoups: nbCoups,
+                    nbPoints: nbPoints,
+                })
+            })
+        })
+
+        const groupedScoresByYear = _.groupBy(scoresArray, 'AnneeSaison');
+        const finalArray = _.values(groupedScoresByYear);
+
+        return finalArray;
+    },[playerScoresSortedByDay]
+    ) 
 
 
     // Fonctions GET
@@ -59,7 +181,7 @@ function TournamentRanking () {
         const specificTournament = []
 
         tournaments.map((tournament) => {
-            if(tournament.IdTournoi.includes(idTournoi)){
+            if(tournament.IdTournoi === (idTournoi)){
                 specificTournament.push(tournament)
             }
         })
@@ -70,47 +192,58 @@ function TournamentRanking () {
         const specificPlayer = [] 
 
         players.map((player) => {
-            if(player.IdJoueur.includes(idJoueur)){
+            if(player.idJoueur === idJoueur){
                 specificPlayer.push(player)
             }
         })
         return specificPlayer;
     }
 
+    function getPlayerScoresByDay(index, idJoueur) {
+        const dayScores = [];
 
-
-    function getCountWins(idJoueur) {
-
-        let countWins = 0;
-        const checkedArrays = []
-      
-        // parcours de tout les tournois joués
-        tournaments.map((tournament) => {
-
-            const scoresByTournament = filterPlayerStatsByYear(tournament.IdTournoi);
-            const sameElt = _.find(checkedArrays, {'AnneeSaison': tournament.AnneeSaison, 'IdTournoi': tournament.IdTournoi});
-
-            if (typeof sameElt === 'undefined'){
-
-                // parcours des scores du tournoi par année (2022 puis 2023,...)
-                scoresByTournament.map((score) => {
-
-                    const tournamentWinnerID = score[0].IdJoueur;
-
-                    // Vérification si l'id en paramètre est celui du 1er du tournoi (celui d'index 0) 
-                    if(tournamentWinnerID === idJoueur){
-                        countWins = countWins + 1;
-                        checkedArrays.push(score[0]) 
-                    }else{
-                        checkedArrays.push(score[0]) 
-                    }
-                })
+        playerScoresSortedByDayAdded[index].map((day) => {
+            if(day.IdJoueur === idJoueur){
+                dayScores.push(day);
             }
         })
-        return countWins;
+
+        return dayScores;
     }
 
+    
 
+
+    // function getCountWins(idJoueur) {
+
+    //     let countWins = 0;
+    //     const checkedArrays = []
+      
+    //     // parcours de tout les tournois joués
+    //     tournaments.map((tournament) => {
+
+    //         const scoresByTournament = filterPlayersScoreByYear(tournament.IdTournoi);
+    //         const sameElt = _.find(checkedArrays, {'AnneeSaison': tournament.AnneeSaison, 'IdTournoi': tournament.IdTournoi});
+
+    //         if (typeof sameElt === 'undefined'){
+
+    //             // parcours des scores du tournoi par année (2022 puis 2023,...)
+    //             scoresByTournament.map((score) => {
+
+    //                 const tournamentWinnerID = score[0].IdJoueur;
+
+    //                 // Vérification si l'id en paramètre est celui du 1er du tournoi (celui d'index 0) 
+    //                 if(tournamentWinnerID === idJoueur){
+    //                     countWins = countWins + 1;
+    //                     checkedArrays.push(score[0]) 
+    //                 }else{
+    //                     checkedArrays.push(score[0]) 
+    //                 }
+    //             })
+    //         }
+    //     })
+    //     return countWins;
+    // }
 
 
 
@@ -127,14 +260,16 @@ function TournamentRanking () {
     // console.log(tournamentsInfo)
     // console.log(tournaments)
     // console.log(playerStats)
-    //  console.log(filteredPlayerStats)
+    // console.log(filteredPlayerStats)
 
     // console.log(activeCategory)
+
+    console.log(playerStats);
 
     return (
         <div className='tournamentRanking'>
             <div className='inner-container'>
-                <h1 className='main-title'> Classements par tournoi </h1>
+                <h1 className='main-title'> Classements par tournoi catégorie {gender}</h1>
 
                 <ul className='separator-list'>
                     <li></li>
@@ -144,13 +279,14 @@ function TournamentRanking () {
 
                 <div className='radio-container'>
                     {tournamentsInfo.map((tournament) => {
+
                         return (
-                            <div>
+                            <div key={tournament.IdTournoi}>
                                 <label 
                                     for={tournament.nomTournoi}
-                                    style={activeCategory === tournament.idTournoi ? {backgroundColor: "grey", color: "white"} : {backgroundColor: "rgb(214, 214, 214)"}}
+                                    style={activeCategory === tournament.IdTournoi ? {backgroundColor: "grey", color: "white"} : {backgroundColor: "rgb(214, 214, 214)"}}
                                 > 
-                                    {activeCategory === tournament.idTournoi ? <i class='bx bx-check'></i> : null }
+                                    {activeCategory === tournament.IdTournoi ? <i class='bx bx-check'></i> : null }
                                     {tournament.nomTournoi} 
                                 </label>
                                 <input 
@@ -159,7 +295,7 @@ function TournamentRanking () {
                                     name="tournament" 
                                     value={tournament.nomTournoi} 
                                     onChange={() => {
-                                        handleRadio(tournament.idTournoi)
+                                        setActiveCategory(tournament.IdTournoi)
                                     }}
                                 />
                             </div>
@@ -170,14 +306,17 @@ function TournamentRanking () {
 
                 {activeCategory ? (
                     <React.Fragment>
-                        {filteredPlayerStats.map((year,index) => {
-                        
-                            const infoTournoi = getInfosTournoi(year[0].IdTournoi);
+
+                        {/* Parcours des saisons du tournoi selectionné */}
+                        {sumOfScores.map((scoresOfYear,index) => {
+                  
+                            const infoTournoi = getInfosTournoi(scoresOfYear[0].IdTournoi);
 
                             const idTournoi = infoTournoi[index].IdTournoi;
                             const anneeSaison = infoTournoi[index].AnneeSaison;
-                            const tournamentKey = idTournoi.concat('-',anneeSaison);
+                            const tournamentKey = idTournoi+'-'+anneeSaison;
 
+                    
                             return (
                                 <div className='TR-table' key={tournamentKey} >
                                     <div className='TR-head'>
@@ -190,22 +329,38 @@ function TournamentRanking () {
                                             <h4>Nombre de coups</h4>
                                             <h4>Score</h4>
                                         </div>
-
                                     </div>
 
                                     <div className='TR-body'>
-                                        {year.map((player,rank) => {
+                                        {/* Parcours des scores par année du tournoi selectionné */}
+                                        {scoresOfYear.map((playerScore,rank) => {
 
-                                            const playerInfoArray = getInfosPlayer(player.IdJoueur);
+                                            const playerInfoArray = getInfosPlayer(playerScore.IdJoueur);
                                             const playerInfo = playerInfoArray[0];
 
+
+                                            const dayScoresOfPlayer = getPlayerScoresByDay(index,playerScore.IdJoueur);
+
                                             return (
-                                                <div className='TR-row'>
-                                                    <h4><strong>{rank+1}</strong></h4>
-                                                    <h4> {playerInfo.nom} {playerInfo.prenom} </h4>
-                                                    <h4> {player.nombreCoups} </h4>
-                                                    <h4> {player.nombrePoints} </h4>
-                                                </div> 
+                                                <React.Fragment>
+                                                    <HtmlTooltip
+                                                        title={
+                                                            dayScoresOfPlayer.map((day) => {
+                                                                return (
+                                                                    <p> Jour {day.jour} : {day.nbPoints} </p>
+                                                                )
+                                                            })
+                                                        }
+                                                    >
+                                                    <div className='TR-row'>
+                                                        <h4><strong>{rank+1}</strong></h4>
+                                                        <h4> {playerInfo.nom} {playerInfo.prenom} </h4>
+                                                        <h4> {playerScore.nbCoups} </h4>
+                                                        <h4> {playerScore.nbPoints} </h4>
+                                                
+                                                    </div> 
+                                                    </HtmlTooltip>
+                                                </React.Fragment>
                                             )
                                         })}
                                     </div>
@@ -225,5 +380,7 @@ function TournamentRanking () {
         </div>         
     )
 };
+
+
 
 export default TournamentRanking;

ea5217c4c8fdc048c68f67266b8183d1f84df087
Pascal <pppascallim@gmail.com>
2022-04-05 13:35:05 +0200

    Last changes on routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState, useEffect  } from 'react';
+import React, { useState, useEffect  } from 'react';
 import { LoginContext } from './components/Helper/LoginContext';
+import Axios from 'axios';
 
 import Navbar from './components/main/Navbar.jsx';
 import Home from './components/main/Home.js';
@@ -10,36 +11,53 @@ import TournamentRanking from './components/main/TournamentRanking';
 import PlayerRanking from './components/main/PlayerRanking';
 import Players from './components/main/Players';
 import Tournaments from './components/main/Tournaments';
-import Login from './components/main/Login';
-import AddAdmin from './components/main/AddAdmin';
- 
-
+import Login from './components/main/admin/Login';
+import AdminRegister from './components/main/admin/AdminRegister';
+import AdminAddScores from './components/main/admin/AdminAddScores';
+import AdminAddTournaments from './components/main/admin/AdminAddTournaments';
+import AdminPlayerList from './components/main/admin/AdminPlayerList';
+ import AdminPlayerAdd from './components/main/admin/AdminPlayerAdd';
+import AdminPlayerEdit from './components/main/admin/AdminPlayerEdit';
+import ProtectedRoute from './components/main/ProtectedRoute';
 
 function App() {
 
-  const [ isLoggedIn , setIsLoggedIn] = useState(false)
+  const [ isLoggedIn , setIsLoggedIn] = useState(null);
+
+  // useEffect(async() => {
+  //   const response = await Axios.get('http://localhost:3001/admin/login');
+
+  //   setIsLoggedIn(response.data.loggedIn);
+    
+  // }, [])
+  // console.log("app.js isLoggedIn", isLoggedIn);
+
 
   return (
     <BrowserRouter>
-      <div id="root" >
-        <LoginContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
-          <Navbar />
+      <div id="root">
+          <LoginContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+            <Navbar />
+            
+            <Routes>        
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route exact path="playerRanking" element={<PlayerRanking />} />
+              <Route exact path="/tournamentRanking/:gender" element={<TournamentRanking />} />
+              <Route exact path="/playerStats" element={<Players />} />
+              <Route exact path="/tournaments" element={<Tournaments />} />
 
-          <Routes>        
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/dashboard" element={<Dashboard />} />
-            <Route exact path="playerRanking" element={<PlayerRanking />} />
-            <Route exact path="/tournamentRanking" element={<TournamentRanking />} />
-            <Route exact path="/playerStats" element={<Players />} />
-            <Route exact path="/tournaments" element={<Tournaments />} />
+              <Route exact path="/admin/player/list" element={<ProtectedRoute> <AdminPlayerList/> </ProtectedRoute>} />
+              <Route exact path="/admin/player/add" element={<ProtectedRoute> <AdminPlayerAdd /> </ProtectedRoute>} />
+              <Route exact path="/admin/player/:idJoueur/edit" element={<ProtectedRoute> <AdminPlayerEdit /> </ProtectedRoute>} />
+              <Route exact path="/admin/tournament/create" element={<ProtectedRoute> <AdminAddTournaments /> </ProtectedRoute>} />
+              <Route exact path="/admin/tournament/addScores" element={<ProtectedRoute> <AdminAddScores /> </ProtectedRoute>} />
+              <Route exact path='/admin/register' element={<ProtectedRoute> <AdminRegister /> </ProtectedRoute>} />
+              <Route exact path="/admin/login" element={<Login />} />
 
-            {/* <Route exact path="/admin/players/list" element={} /> */}
-            <Route exact path='/admin/addAdmin' element={<AddAdmin />} />
-            <Route exact path="/admin/login" element={<Login />} />
-
-            <Route path="*" element={<Dashboard />} />
-          </Routes>
-        </LoginContext.Provider>
+              <Route path="*" element={<Dashboard />} />
+            </Routes>
+          </LoginContext.Provider>
       </div>
     </BrowserRouter>
   );
